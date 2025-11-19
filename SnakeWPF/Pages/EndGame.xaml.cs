76f7ad837bb3087345c66ddb4ab619b6ee078dba
Masshh <msipicyna96@gmail.com>
2025-11-20 01:18:42 +0500

    Прописаны конструктор и метод для страницы EndGame

--- SnakeWPF/Pages/EndGame.xaml.cs
+++ SnakeWPF/Pages/EndGame.xaml.cs
@@ -23,6 +23,29 @@ namespace SnakeWPF.Pages
         public EndGame()
         {
             InitializeComponent();
+            // Выводим наименование игрока
+            name.Content = MainWindow.mainWindow.ViewModelUserSettings.Name;
+            // Выводим место игрока среди всего списка
+            top.Content = MainWindow.mainWindow.ViewModelGames.Top;
+            // Выводим сколько очков набрал игрок
+            glasses.Content = $"{MainWindow.mainWindow.ViewModelGames.SnakesPlayers.Points.Count - 3} glasses";
+            // Закрываем соединение
+            MainWindow.mainWindow.receivingUdpClient.Close();
+            // останавливаем поток
+            MainWindow.mainWindow.tRec.Abort();
+            // Обнуляем данные о змее
+            MainWindow.mainWindow.ViewModelGames = null;
+        }
+
+        /// <summary>
+        /// Переход на начальную страницу
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenHome(object sender, RoutedEventArgs e)
+        {
+            // открываем начальную страницу
+            MainWindow.mainWindow.OpenPage(MainWindow.mainWindow.Home);
         }
     }
 }
